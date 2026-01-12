bf4be4aa11c4170b8960be9166230481b6dbe281
pepijn-devries <peppi2@gmail.com>
2026-01-12 11:54:46 +0100

    Improved test coverage. Some small fixes

--- tests/testthat/test-exceptions.R
+++ tests/testthat/test-exceptions.R
@@ -10,3 +10,25 @@ test_that("Non existing collections err", {
     dse_stac_collections("foobar")
   }, "NotFoundError")
 })
+
+test_that("Non existing stac filter argument will produce warning", {
+  skip_if_offline()
+  expect_warning({
+    dse_stac_search_request(foobar = NA)
+  }, "Ignoring unknown filter arguments")
+})
+
+test_that("Files cannot be downloaded via STAC when authentication is missing", {
+  skip_if_offline()
+  expect_error({
+    fn <-
+      dse_stac_download(
+        id = "S2A_MSIL1C_20260109T132741_N0511_R024_T39XVL_20260109T142148",
+        asset = "B01",
+        destination = tempdir(),
+        s3_key = "",
+        s3_secret = "",
+        token = NULL
+      )
+  }, "Need authentication")
+})
