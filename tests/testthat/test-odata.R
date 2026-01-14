b52494d95e13005219c5699f9bc4e237e0806ca9
pepijn-devries <peppi2@gmail.com>
2026-01-14 10:28:17 +0100

    Added tests

--- tests/testthat/test-odata.R
+++ tests/testthat/test-odata.R
@@ -46,3 +46,33 @@ test_that("OData attributes can be listed", {
       all(names(attribs) %in% c("Collection", "AttributeName", "AttributeValueType"))
   })
 })
+
+test_that("OData file can be downloaded through https", {
+  skip_if_offline()
+  skip_on_cran()
+  expect_no_error({
+    resp <-
+      dse_odata_download_path(
+        product     = "2f497806-0101-5eea-83fa-c8f68bc56b0c",
+        node_path   =
+          paste("DEM1_SAR_DTE_90_20101213T034716_20130408T035028_ADS_000000_5033.DEM",
+                "Copernicus_DSM_30_S09_00_E026_00", "DEM",
+                "Copernicus_DSM_30_S09_00_E026_00_DEM.dt1", sep = "/"),
+        destination = tempdir()
+      )
+    on.exit({unlink(resp$body)})
+    sp <- read_stars(resp$body)
+  })
+})
+
+test_that("OData zipped product can be downloaded through https", {
+  skip_if_offline()
+  skip_on_cran()
+  expect_no_error({
+    resp <-
+      dse_odata_download_path(
+        product     = "ce4576eb-975b-40ff-8319-e04b00d8d444",
+        destination = tempdir())
+    unlink(resp$body)
+  })
+})
