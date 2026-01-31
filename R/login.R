f2623721cb486ced098dd7fcbece8467d3646105
pepijn-devries <peppi2@gmail.com>
2026-02-01 00:04:51 +0100

    Use env vars to support GDAL access to online resources

--- R/login.R
+++ R/login.R
@@ -236,3 +236,33 @@ dse_s3_secret <- function(...) {
 `dse_s3_secret<-` <- function(..., value) {
   Sys.setenv(CDSE_API_S3SECRET = as.character(value))
 }
+
+#' Set Copernicus Data Space Ecosystem Access Token for GDAL Driver
+#' 
+#' This function sets system environment variables, such
+#' that the GDAL library can access the Copernicus Data Space
+#' Ecosystem https storage. Note that these settings can be used
+#' by any package depending on the GDAL library. Most notably:
+#' `stars`, `terra`, and `gdalraster`.
+#' @inheritParams dse_usage
+#' @returns Returns a `logical` value. `TRUE` if all variables
+#' were successfully set. `FALSE` otherwise.
+#' @examples
+#' if (interactive() && dse_has_client_info() &&
+#'     requireNamespace("stars")) {
+#'   uri <-
+#'     dse_stac_get_uri(
+#'       "S2A_MSIL1C_20260109T132741_N0511_R024_T39XVL_20260109T142148",
+#'       "B01", type = "odata")
+#'    
+#'   dse_set_gdal_token()
+#'
+#'   ## As this URI is zipped, it need to be downloaded.
+#'   ## But you can access it directly:
+#'   jp2 <- stars::read_stars(uri)
+#' }
+#' @export
+dse_set_gdal_token  <- function(token = dse_access_token()) {
+  Sys.setenv(GDAL_HTTP_AUTH = "BEARER") &&
+    Sys.setenv(GDAL_HTTP_BEARER = token$access_token)
+}
--- R/s3.R
+++ R/s3.R
@@ -58,3 +58,71 @@ dse_s3_download <- function(
   ds3 <- dse_s3(s3_key = s3_key, s3_secret = s3_secret)
   .download_s3(uri, destination, ds3) |> unlist()
 }
+
+#' Convert Uniform Resource Identifier to Virtual System Identifier
+#' 
+#' Convert Uniform Resource Identifier (URI) to Virtual System
+#' Identifier (VSI). The Copernicus Data Space Ecosystem returns
+#' URIs for accessing assets. Packages that use the GDAL library
+#' (e.g., `stars`, `terra` and `gdalraster`)
+#' can use VSI to access raster data directly. Use this function
+#' to convert such an URI to a VIS.
+#' @param uri A Uniform Resource Identifier, pointing to an
+#' S3 storage file. You can retrieve one with [dse_stac_get_uri()].
+#' @returns A `character` string representing the VSI
+#' @examples
+#' if (interactive()) {
+#'   dse_stac_get_uri(
+#'     "S1A_IW_GRDH_1SDV_20241125T055820_20241125T055845_056707_06F55C_12F9_COG",
+#'     "vh", "sentinel-1-grd") |>
+#'     dse_s3_uri_to_vsi()
+#' }
+#' @export
+dse_s3_uri_to_vsi <- function(uri) {
+  gsub("s3://", "/vsis3/", uri)
+}
+
+#' Set-up S3 Configuration for GDAL Library
+#' 
+#' This function sets system environment variables, such
+#' that the GDAL library can access the Copernicus Data Space
+#' Ecosystem S3 storage. Note that these settings can be used
+#' by any package depending on the GDAL library. Most notably:
+#' `stars`, `terra`, and `gdalraster`.
+#' @param ... Ignored
+#' @inheritParams dse_s3
+#' @returns Returns a `logical` value. `TRUE` if all variables
+#' were successfully set. `FALSE` otherwise.
+#' @examples
+#' if (interactive() && dse_s3_has_secret() &&
+#'     requireNamespace("stars")) {
+#'   library(dplyr)
+#'
+#'   ## Get a Virtual System Interface to a tiff file:
+#'   vsi <-
+#'     dse_stac_get_uri(
+#'       "S1A_IW_GRDH_1SDV_20241125T055820_20241125T055845_056707_06F55C_12F9_COG",
+#'       "vh", "sentinel-1-grd") |>
+#'       dse_s3_uri_to_vsi()
+#' 
+#'   ## Make sure to set gdal options with required S3 settings
+#'   dse_s3_set_gdal_options()
+#'    
+#'   ## You can now read the file directly from the online storage
+#'   ## without having to download it completely:
+#'   cog <- stars::read_stars(vsi)
+#'    
+#'   ## You can also easily plot a downsampled version
+#'   plot(cog, downsample = 50)
+#' }
+#' @export
+dse_s3_set_gdal_options <- function(
+    region = "us-east-1", ...,
+    s3_key = dse_s3_key(), s3_secret = dse_s3_secret()) {
+  Sys.setenv(AWS_REGION            = region) &&
+    Sys.setenv(AWS_ACCESS_KEY_ID     = s3_key) &&
+    Sys.setenv(AWS_SECRET_ACCESS_KEY = s3_secret) &&
+    Sys.setenv(AWS_VIRTUAL_HOSTING   = "FALSE") &&
+    Sys.setenv(AWS_S3_ENDPOINT       =
+                 gsub("^https://", "", .odata_s3_endpoint))
+}
